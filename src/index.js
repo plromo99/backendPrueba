c3b1a3338745186731c8a3f698540629bd177dd5
plromo99 <paoromog@hotmail.com>
2023-08-17 03:18:41 -0500

    v2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+
+app.use(cors());
 //definir middelwares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 //exportar rutas 
 app.use(require('./rutas/index_rutas'));
+
 
 app.listen(3000);  
 console.log('Server on port 3000'); 
